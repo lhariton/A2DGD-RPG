b6c99c923fb7a6c51dae6401a59f7a031e98d14e
Leonard Hariton <leohariton@gmail.com>
2024-05-20 17:50:21 +0300

    death animation

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 @onready var anim_tree = get_node("AnimationTree")
-var attacking = false
+var attacking: bool = false
+var alive: bool = true
+var health: int = 10
 
 func _physics_process(delta):
 	
@@ -9,7 +11,7 @@ func _physics_process(delta):
 		attacking = true
 		anim_tree.get("parameters/playback").travel("Attack")
 		
-	if attacking == false:
+	if (attacking == false) and (alive == true):
 		var input_vector_x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 		var input_vector_y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 		var input_vector = Vector2(input_vector_x, input_vector_y).normalized()
@@ -22,9 +24,19 @@ func _physics_process(delta):
 			anim_tree.set("parameters/Idle/BlendSpace2D/blend_position", input_vector)
 			anim_tree.set("parameters/Attack/BlendSpace2D/blend_position", input_vector)
 			anim_tree.set("parameters/Walk/BlendSpace2D/blend_position", input_vector)
+			anim_tree.set("parameters/Death/BlendSpace2D/blend_position", input_vector)
 		move_and_slide()
 	
 
 func _on_animation_tree_animation_finished(anim_name):
 	if "Attack_" in anim_name:
 		attacking = false
+		
+func hit(damage):
+	health -= damage
+	if health <=0:
+		alive = false
+		anim_tree.get("parameters/playback").travel("Death")
+		await anim_tree.animation_finished
+		self.queue_free()
+		
