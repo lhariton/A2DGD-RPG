47a57fa4001d8c26874a3645959232f50c3fa581
Leonard Hariton <leohariton@gmail.com>
2024-05-30 15:29:31 +0300

    shuriken does player dmg

--- Scripts/Stuff/projectile.gd
+++ Scripts/Stuff/projectile.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 var speed: int = 1000
+var dmg: int = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("default")
+	dmg = get_node("../Player").damage
 
 func _physics_process(delta):
 	var direction = Vector2.RIGHT.rotated(rotation)
@@ -19,7 +21,7 @@ func _process(delta):
 
 func _on_body_entered(body):
 	if body.is_in_group("Monster"):
-		body.hit(5)
+		body.hit(dmg)
 	#destroy()
 	
 
